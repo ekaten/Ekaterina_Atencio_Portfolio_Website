10a74ac0993bbf9a11a1bb0ceed55c1271d04f11
ekaten <ekaterinaatencio@gmail.com>
2024-04-12 16:38:51 -0400

    Release Version 1.9.2 (add .gitignore and save keys to env)

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 
-
 class Config(object):
-    RECAPTCHA_PUBLIC_KEY = os.environ.get('PUBLIC_KEY') or 'you-can-try-to-guess'
-    RECAPTCHA_PRIVATE_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    RECAPTCHA_PUBLIC_KEY = os.environ.get("PUBLIC_KEY")
+    RECAPTCHA_PRIVATE_KEY = os.environ.get("SECRET_KEY")
